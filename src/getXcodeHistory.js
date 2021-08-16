5c70200a450e19b2627d8f52ec447bd9c7d42bf5
joys <send_out_box@163.com>
2021-08-16 17:37:58 +0800

    不常用xcode，使用缓存

--- src/getXcodeHistory.js
+++ src/getXcodeHistory.js
@@ -8,14 +8,22 @@ function getXcodeHistory() {
     let filePath = "/Library/Application\ Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.apple.dt.xcode.sfl2"
     filePath = config.home.concat(filePath)
     try {
-        fs.statSync(filePath)
-    } catch (e) {
+        const stat = fs.statSync(filePath)
+        const lastTime = utools.dbStorage.getItem("lastTime")
+        // 不常使用，使用数据库记录(缓存)
+        if (stat && stat.mtimeMs === lastTime) {
+            return utools.dbStorage.getItem("stdout")
+        } else {
+            utools.dbStorage.setItem("lastTime", stat.mtimeMs)
+        }
+    } catch (e) {   // 不使用 xcode 直接跳过
         return [];
     }
 
     const cmd = __dirname + '/readXcode'
     let stdout = execFileSync(cmd);
     stdout = JSON.parse(stdout.toString().replaceAll("'", '"'))
+    utools.dbStorage.setItem("stdout", stdout)
     // console.log(stdout)
     return stdout;
 }
