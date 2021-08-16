d36366da1ebdd32e28be4558871a0bc81d8c36b6
joys <send_out_box@163.com>
2021-08-16 17:21:55 +0800

    如果没有使用xcode，则跳过xcode的查询

--- src/getXcodeHistory.js
+++ src/getXcodeHistory.js
@@ -1,7 +1,18 @@
 const { execFileSync } = require('child_process');
+const config = require('./config')
+const fs = require('fs')
 
 
 function getXcodeHistory() {
+    // 判断 com.apple.dt.xcode.sfl2 文件是否存在
+    let filePath = "/Library/Application\ Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.apple.dt.xcode.sfl2"
+    filePath = config.home.concat(filePath)
+    try {
+        fs.statSync(filePath)
+    } catch (e) {
+        return [];
+    }
+
     const cmd = __dirname + '/readXcode'
     let stdout = execFileSync(cmd);
     stdout = JSON.parse(stdout.toString().replaceAll("'", '"'))
